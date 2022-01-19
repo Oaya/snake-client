ec1bb331960aedd66ff1df7b9c19fb0ee7d91b39
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-19 23:57:29 +0000

    create input file

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const { connect } = require("../snake-client/client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-setupInput();
 connect();
+setupInput();
